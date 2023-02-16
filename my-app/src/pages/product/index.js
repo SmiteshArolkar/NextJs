72eadeea22c21c376526dd869b5fb720447779c8
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-02-16 11:54:30 +0530

    replace prop to new state instead of previous call stack

--- my-app/src/pages/product/index.js
+++ my-app/src/pages/product/index.js
@@ -11,7 +11,7 @@ function Product({ id = 100 }) {
       <Link href="/product/2" style={{ margin: 30, padding: 20 }}>
         <h3>Product2</h3>
       </Link>
-      <Link href="/product/3" style={{ margin: 30, padding: 20 }}>
+      <Link href="/product/3" replace style={{ margin: 30, padding: 20 }}>
         <h3>Product3</h3>
       </Link>
       <Link href={`/product/${id}`} style={{ margin: 30, padding: 20 }}>
