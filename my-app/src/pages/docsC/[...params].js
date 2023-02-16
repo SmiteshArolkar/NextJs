cb463b20b31acf95a7446a82f965102ff799ce6b
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-02-16 11:13:01 +0530

    using router to fetch params

--- my-app/src/pages/docsC/[...params].js
+++ my-app/src/pages/docsC/[...params].js
@@ -1,5 +1,10 @@
+import { useRouter } from "next/router";
+
 function Doc_All()
 {
+    const router = useRouter();
+    const { params } = router.query
+    console.log(params)
     return <h1>Docs Home Page</h1>
 }
 
