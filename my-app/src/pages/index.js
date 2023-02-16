15c3fbef62dfde409edd151a601abf1c9060595c
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-02-16 11:32:10 +0530

    Navigation Using <Link>

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Hello World</h1>;
+  return(
+  <>
+    <h1>Hello World</h1>
+    <Link href = "/blog">
+    <h1>Blog</h1>
+    </Link>
+  </>
+  );
 }
 
 export default Home;
