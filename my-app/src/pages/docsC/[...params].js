880f8f8ffe8fdc3cb9e968bfc0d1d640af551823
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-02-16 11:17:59 +0530

    logic

--- my-app/src/pages/docsC/[...params].js
+++ my-app/src/pages/docsC/[...params].js
@@ -1,11 +1,15 @@
 import { useRouter } from "next/router";
 
-function Doc_All()
-{
-    const router = useRouter();
-    const { params } = router.query
-    console.log(params)
-    return <h1>Docs Home Page</h1>
+function Doc_All() {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  console.log(params);
+
+  if (params.length == 2) 
+    return <h1>Viewing Docs for feature : {params[0]} and Concept : {params[1]}</h1>
+   else if (params.length == 1)
+    return <h1>Viewing Docs for feature : {params[0]}</h1>;
+  return <h1>Docs Home Page</h1>;
 }
 
 export default Doc_All;
