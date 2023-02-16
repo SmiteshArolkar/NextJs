b2e8ba9359965383b2b0e88a3ff11471035b6942
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-02-16 11:09:09 +0530

    Catching All routes

---
+++ my-app/src/pages/docsC/[...params].js
@@ -0,0 +1,6 @@
+function Doc_All()
+{
+    return <h1>Docs Home Page</h1>
+}
+
+export default Doc_All;
---
+++ my-app/src/pages/docsC/index.js
@@ -0,0 +1,6 @@
+function Doc_All()
+{
+    return <h1>Docs Home Page Index</h1>
+}
+
+export default Doc_All;
