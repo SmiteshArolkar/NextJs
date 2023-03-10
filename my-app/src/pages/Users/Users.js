3d0ee7915d1c4b723264f19a6715e9986ca4e9f7
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-03-10 11:40:52 +0530

    fetching Json Data

--- my-app/src/pages/Users/Users.js
+++ my-app/src/pages/Users/Users.js
@@ -1,4 +1,10 @@
+async function getStaticProps(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data);
+}
 function UserList(){
+    getStaticProps();
     return <h1>List of Users</h1>
 }
 
